8e65c523d3b184cca9831e10ad33485607e0c72e
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-12 10:50:40 -0300

    Partial client setup

--- lib/zenaton/client.rb
+++ lib/zenaton/client.rb
@@ -1,14 +1,131 @@
 # frozen_string_literal: true
 
 require 'singleton'
+require 'zenaton/services/http'
+require 'zenaton/workflow'
 
 module Zenaton
   # Zenaton Client
   class Client
     include Singleton
+    ZENATON_API_URL = 'https://zenaton.com/api/v1'
+    ZENATON_WORKER_URL = 'http://localhost'
+    DEFAULT_WORKER_PORT = 4001
+    WORKER_API_VERSION = 'v_newton'
+
+    MAX_ID_SIZE = 256
+
+    APP_ENV = 'app_env'
+    APP_ID = 'app_id'
+    API_TOKEN = 'api_token'
+
+    ATTR_ID = 'custom_id'
+    ATTR_NAME = 'name'
+    ATTR_CANONICAL = 'canonical_name'
+    ATTR_DATA = 'data'
+    ATTR_PROG = 'programming_language'
+    ATTR_MODE = 'mode'
+
+    PROG = 'RUBY'
+
+    EVENT_INPUT = 'event_input'
+    EVENT_NAME = 'event_name'
+
+    WORKFLOW_KILL = 'kill'
+    WORKFLOW_PAUSE = 'pause'
+    WORKFLOW_RUN = 'run'
+
+    attr_writer :app_id, :api_token, :app_env
+
+    def self.init(app_id, api_token, app_env)
+      instance.tap do |client|
+        client.app_id = app_id
+        client.api_token = api_token
+        client.app_env = app_env
+      end
+    end
+
+    # @private
+    def initialize
+      @http = Services::Http.new
+    end
+
+    # Gets the url for the workers
+    # @param resource [String] the endpoint for the worker
+    # @param params [String] url encoded parameters to include in request
+    # @return [String] the workers url with parameters
+    def get_worker_url(resource = '', params = '')
+      base_url = ENV['ZENATON_WORKER_URL'] || ZENATON_WORKER_URL
+      port = ENV['ZENATON_WORKER_PORT'] || DEFAULT_WORKER_PORT
+      url = "#{base_url}:#{port}/api/#{WORKER_API_VERSION}/#{resource}?"
+      add_app_env(url, params)
+    end
+
+    # Gets the url for zenaton api
+    # @param resource [String] the endpoint for the api
+    # @param params [String] url encoded parameters to include in request
+    # @return [String] the api url with parameters
+    def get_website_url(resource = '', params = '')
+      api_url = ENV['ZENATON_API_URL'] || ZENATON_API_URL
+      url = "#{api_url}/#{resource}?#{API_TOKEN}=#{@api_token}&"
+      add_app_env(url, params)
+    end
 
     # Start the specified workflow
     # @param workflow [Zenaton::Workflow]
-    def start_workflow(workflow); end
+    def start_workflow(flow)
+      custom_id = parse_custom_id_from(flow)
+      @http.post(
+        get_instance_worker_url,
+        ATTR_PROG => PROG,
+        ATTR_CANONICAL => nil,
+        ATTR_NAME => get_class(flow),
+        ATTR_DATA => { hard_coded: 'json' }.to_json,
+        ATTR_ID => custom_id
+      )
+    end
+
+    private
+
+    def add_app_env(url, params)
+      app_env = @app_env ? "#{APP_ENV}=#{@app_env}&" : ''
+      app_id = @app_id ? "#{APP_ID}=#{@app_id}&" : ''
+
+      "#{url}#{app_env}#{app_id}#{params}"
+    end
+
+    def get_instance_website_url(params)
+      get_website_url('instances', params)
+    end
+
+    def get_instance_worker_url(params = '')
+      get_worker_url('instances', params)
+    end
+
+    def get_send_url # rubocop:disable Naming/AccessorMethodName
+      get_worker_url('events')
+    end
+
+    def get_class(object)
+      object.class.name
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def parse_custom_id_from(flow)
+      custom_id = flow.get_id
+      if custom_id
+        unless custom_id.is_a?(String) || custom_id.is_a?(Integer)
+          raise InvalidArgumentError,
+                'Provided ID must be a string or an integer'
+        end
+        custom_id = custom_id.to_s
+        if custom_id.length > MAX_ID_SIZE
+          raise InvalidArgumentError,
+                "Provided Id must not exceed #{MAX_ID_SIZE} bytes"
+        end
+      end
+      custom_id
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
--- spec/zenaton/client_spec.rb
+++ spec/zenaton/client_spec.rb
@@ -4,6 +4,40 @@ require 'zenaton/client'
 
 RSpec.describe Zenaton::Client do
   let(:client) { described_class.instance }
+  let(:http) { instance_double(Zenaton::Services::Http, post: nil) }
+  let(:workflow) do
+    instance_double(
+      Zenaton::Workflow,
+      get_id: custom_id,
+      class: Zenaton::Workflow
+    )
+  end
+
+  before do
+    setup_client
+  end
+
+  describe '::init' do
+    let(:instance) { described_class.init('AppId', 'SecretToken', 'AppEnv') }
+
+    before { instance }
+
+    it 'returns the instance of the class' do
+      expect(instance).to eq(client)
+    end
+
+    it 'sets the app id' do
+      expect(instance.instance_variable_get(:@app_id)).to eq('AppId')
+    end
+
+    it 'sets the api token' do
+      expect(instance.instance_variable_get(:@api_token)).to eq('SecretToken')
+    end
+
+    it 'sets the app environment' do
+      expect(instance.instance_variable_get(:@app_env)).to eq('AppEnv')
+    end
+  end
 
   describe 'initialization' do
     it 'there is only a single instance of the class' do
@@ -15,5 +49,155 @@ RSpec.describe Zenaton::Client do
       expect { described_class.new }.to \
         raise_error NoMethodError, /private method `new' called/
     end
+
+    it 'stores an instance of the http service as an instance variable' do
+      expect(client.instance_variable_get(:@http)).to eq(http)
+    end
+  end
+
+  describe '#get_worker_url' do
+    context 'with environment variables but no instance variables set' do
+      around do |example|
+        ENV['ZENATON_WORKER_URL'] = 'http://192.168.1.1'
+        ENV['ZENATON_WORKER_PORT'] = '42'
+        example.run
+        ENV.delete('ZENATON_WORKER_URL')
+        ENV.delete('ZENATON_WORKER_PORT')
+      end
+
+      it 'returns the worker url with params' do
+        url = client.get_worker_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('http://192.168.1.1:42/api/v_newton/my_resource?myParam=1')
+      end
+    end
+
+    context 'with environment and instances variables set' do
+      before { described_class.init('AppId', 'ApiToken', 'AppEnv') }
+
+      around do |example|
+        ENV['ZENATON_WORKER_URL'] = 'http://192.168.1.1'
+        ENV['ZENATON_WORKER_PORT'] = '42'
+        example.run
+        ENV.delete('ZENATON_WORKER_URL')
+        ENV.delete('ZENATON_WORKER_PORT')
+      end
+
+      it 'returns the worker url with params and app env' do
+        url = client.get_worker_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('http://192.168.1.1:42/api/v_newton/my_resource?app_env=AppEnv&app_id=AppId&myParam=1')
+      end
+    end
+
+    context 'with instances variables but no environment variables set' do
+      before { described_class.init('AppId', 'ApiToken', 'AppEnv') }
+
+      it 'returns the default worker url with params and app env' do
+        url = client.get_worker_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('http://localhost:4001/api/v_newton/my_resource?app_env=AppEnv&app_id=AppId&myParam=1')
+      end
+    end
+
+    context 'with no environment nor instances variables set' do
+      it 'returns the default worker url with params' do
+        url = client.get_worker_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('http://localhost:4001/api/v_newton/my_resource?myParam=1')
+      end
+    end
+  end
+
+  describe '#get_website_url' do
+    before { described_class.init('AppId', 'ApiToken', 'AppEnv') }
+
+    context 'with environment variables set' do
+      around do |example|
+        ENV['ZENATON_API_URL'] = 'http://192.168.1.1'
+        example.run
+        ENV.delete('ZENATON_API_URL')
+      end
+
+      it 'returns the website url with params and api token' do
+        url = client.get_website_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('http://192.168.1.1/my_resource?api_token=ApiToken&app_env=AppEnv&app_id=AppId&myParam=1')
+      end
+    end
+
+    context 'with no environment variables set' do
+      it 'returns the default website url with params and api token' do
+        url = client.get_website_url('my_resource', 'myParam=1')
+        expect(url).to \
+          eq('https://zenaton.com/api/v1/my_resource?api_token=ApiToken&app_env=AppEnv&app_id=AppId&myParam=1')
+      end
+    end
+  end
+
+  describe '#start_workflow' do
+    let(:start_workflow) { client.start_workflow(workflow) }
+    let(:expected_url) { 'http://localhost:4001/api/v_newton/instances?' }
+    let(:expected_hash) do
+      {
+        'programming_language' => 'RUBY',
+        'canonical_name' => nil,
+        'name' => 'Zenaton::Workflow',
+        'data' => '{"hard_coded":"json"}',
+        'custom_id' => nil
+      }
+    end
+
+    context 'with an integer custom id' do
+      let(:custom_id) { 123 }
+
+      it 'sends the custom id as a string' do
+        start_workflow
+        expect(http).to have_received(:post)
+          .with(expected_url, hash_including('custom_id' => '123'))
+      end
+    end
+
+    context 'with an string custom id' do
+      let(:custom_id) { 'MyWorkflowId' }
+
+      it 'sends the custom id' do
+        start_workflow
+        expect(http).to have_received(:post)
+          .with(expected_url, hash_including('custom_id' => 'MyWorkflowId'))
+      end
+    end
+
+    context 'with an invalid custom id type' do
+      let(:custom_id) { {} }
+
+      it 'raises an error' do
+        expect { start_workflow }.to \
+          raise_error Zenaton::InvalidArgumentError
+      end
+    end
+
+    context 'with a custom id too long' do
+      let(:custom_id) { 'a' * 300 }
+
+      it 'raises an error' do
+        expect { start_workflow }.to \
+          raise_error Zenaton::InvalidArgumentError
+      end
+    end
+
+    context 'without a custom id' do
+      let(:custom_id) { nil }
+
+      it 'sends a post request to the http client' do
+        start_workflow
+        expect(http).to have_received(:post).with(expected_url, expected_hash)
+      end
+    end
+  end
+
+  def setup_client
+    Singleton.__init__(described_class)
+    allow(Zenaton::Services::Http).to receive(:new).and_return(http)
   end
 end

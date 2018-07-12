d159ab89c44886a6e065b39ee30ba852aa11e066
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-12 19:14:08 -0300

    Amend missing type correction

--- lib/zenaton/processor.rb
+++ lib/zenaton/processor.rb
@@ -4,7 +4,7 @@ module Zenaton
   # Zenaton Processor
   class Processor
     # processes the jobs
-    # @param jobs [Array<Zenaton::Job>]
+    # @param jobs [Array<Zenaton::Interfaces::Job>]
     # @param bool [Boolean]
     def process(jobs, bool); end
   end

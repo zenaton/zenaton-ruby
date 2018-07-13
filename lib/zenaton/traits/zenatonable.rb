67a176520446df503e44ad8d244db58d20744291
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-13 10:50:28 -0300

    Add the where_id method to the zenatonable trait

--- lib/zenaton/traits/zenatonable.rb
+++ lib/zenaton/traits/zenatonable.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'zenaton/engine'
+require 'zenaton/query/builder'
 
 module Zenaton
   # Reusable modules from the Zenaton library
@@ -17,6 +18,10 @@ module Zenaton
       # the result
       def dispatch
         Engine.instance.dispatch([self])[0]
+      end
+
+      def where_id(id)
+        Query::Builder.new(self.class).where_id(id)
       end
     end
   end

3cccaa8ad838fdb2da930107473b3f8ad9af6f72
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-14 09:04:24 -0300

    with_timestamp includes with_duration

---
+++ lib/zenaton/traits/with_timestamp.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'zenaton/traits/with_duration'
+
+module Zenaton
+  module Traits
+    module WithTimestamp
+      include WithDuration
+    end
+  end
+end

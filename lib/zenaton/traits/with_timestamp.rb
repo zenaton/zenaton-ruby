6f1982ad3b12bcb538552256f8b046fd656327ed
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-16 12:31:50 -0300

    WithTimestamp methods

--- lib/zenaton/traits/with_timestamp.rb
+++ lib/zenaton/traits/with_timestamp.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # rubocop:disable Style/ClassVars
-require 'active_support/concern'
+require 'active_support'
 require 'zenaton/exceptions'
 require 'zenaton/traits/with_duration'
 
@@ -11,6 +11,7 @@ module Zenaton
       include WithDuration
       extend ActiveSupport::Concern
 
+      WEEKDAYS = DateAndTime::Calculations::DAYS_INTO_WEEK.keys
       MODE_AT = 'AT'
       MODE_WEEK_DAY = 'WEEK_DAY'
       MODE_MONTH_DAY = 'MONTH_DAY'
@@ -20,6 +21,89 @@ module Zenaton
         @@_timezone = nil
       end
 
+      def _get_timestamp_or_duration
+        return [nil, nil] unless @buffer
+        now, now_dup = _init_now_then
+        @_mode = nil
+        @buffer.each do |time_unit, time_value|
+          now_dup = _apply(time_unit, time_value, now, now_dup)
+        end
+        return [nil, diff_in_seconds(now, now_dup)] if @_mode.nil?
+        [now_dup.to_i, nil]
+      end
+
+      %i[timestamp at on_day monday tuesday wednesday thurday friday saturday sunday]. each do |method_name|
+        define_method method_name do |value = 1|
+          @buffer ||= {}
+          @buffer[method_name] = value
+          self
+        end
+      end
+
+      private
+
+      def _apply(method, value, now, now_dup)
+        if WEEKDAYS.include?(method)
+          _weekday(value, method, now_dup)
+        elsif method == :timestamp
+          _timestamp(value)
+        elsif method == :at
+          _at(value, now, now_dup)
+        elsif method == :on_day
+          _on_day(value, now, now_dup)
+        else
+          _apply_duration(method, value, now_dup)
+        end
+      end
+
+      def _weekday(value, day, now_dup)
+        _set_mode(MODE_WEEK_DAY)
+        value.times { |_n| now_dup = now_dup.next_occurring(day) }
+        now_dup
+      end
+
+      def _timestamp(timestamp)
+        _set_mode(MODE_TIMESTAMP)
+        timestamp
+      end
+
+      def _at(time, now, now_dup)
+        _set_mode(MODE_AT)
+        hour, min, sec = time.split(':').map(&:to_i)
+        now_dup = now_dup.change(hour: hour, min: min, sec: sec)
+        if now > now_dup
+          case @_mode
+          when MODE_AT
+            now_dup += 1.day
+          when MODE_WEEK_DAY
+            now_dup += 1.week
+          when MODE_MONTH_DAY
+            now_dup += 1.month
+          else
+            raise ExternalError "Unknown mode: #{@_mode}"
+          end
+        end
+
+        now_dup
+      end
+
+      def _on_day(day, now, now_dup)
+        _set_mode(MODE_MONTH_DAY)
+        now_dup = now_dup.change(day: day)
+        now_dup += 1.month if now > now_dup
+        now_dup
+      end
+
+      def _set_mode(mode)
+        raise ExternalError if mode == @_mode
+        raise ExternalError if timestamp_mode_set?(mode)
+        @_mode = mode if @_mode.nil? || @_mode == MODE_AT
+      end
+
+      def timestamp_mode_set?(mode)
+        (!@_mode.nil? && MODE_TIMESTAMP == mode) || (@_mode == MODE_TIMESTAMP)
+      end
+
       class_methods do
         def timezone=(timezone)
           error = 'Unknown timezone'
@@ -27,10 +111,8 @@ module Zenaton
           @@_timezone = timezone
         end
 
-        private
-
         def valid_timezone?(timezone)
-          ActiveSupport::TimeZone::MAPPING.value? timezone
+          timezone.nil? || ActiveSupport::TimeZone::MAPPING.value?(timezone)
         end
       end
     end
--- spec/shared_examples/with_timestamp.rb
+++ spec/shared_examples/with_timestamp.rb
@@ -5,6 +5,8 @@ RSpec.shared_examples 'WithTimestamp' do |initial_arg|
   let(:with_timestamp) { klass.new(initial_arg) }
 
   describe 'timezone writer' do
+    after { klass.timezone = nil }
+
     it 'sets the timezone class variable' do
       klass.timezone = 'America/Sao_Paulo'
       expect(klass.class_variable_get(:@@_timezone)).to \
@@ -15,6 +17,77 @@ RSpec.shared_examples 'WithTimestamp' do |initial_arg|
       expect do
         klass.timezone = 'this is not a valid timezone'
       end.to raise_error Zenaton::ExternalError
+    end
+  end
+
+  describe '_get_timestamp_or_duration' do
+    subject { with_timestamp._get_timestamp_or_duration }
+
+    context 'when there is no buffer' do
+      it { is_expected.to eq [nil, nil] }
+    end
+
+    context 'when applying duration methods' do
+      before { with_timestamp.seconds }
+
+      it { is_expected.to eq [nil, 1] }
+    end
+
+    context 'when applying weekday method' do
+      let(:next_monday) { Date.new(2018, 7, 13) + 3.days }
+
+      before { with_timestamp.monday }
+
+      around do |example|
+        # 13/07/2018 was a Friday
+        Timecop.freeze(Date.new(2018, 7, 13))
+        example.run
+        Timecop.return
+      end
+
+      it { is_expected.to eq [next_monday.to_time.to_i, nil] }
+    end
+
+    context 'when applying a timestamp' do
+      before { with_timestamp.timestamp(100) }
+
+      it { is_expected.to eq([100, nil]) }
+    end
+
+    context 'when applying an at hour' do
+      let(:expected_time) { Time.parse('2018-11-04 15:10:23 EST -05:00') }
+
+      before { with_timestamp.at('15:10:23') }
+
+      around do |example|
+        described_class.timezone = 'America/New_York'
+        Timecop.freeze(Time.parse('2018-11-04 01:59:00 EDT -04:00'))
+        expected_time
+        example.run
+        Timecop.return
+        described_class.timezone = nil
+      end
+
+      it { is_expected.to eq([expected_time.to_i, nil]) }
+    end
+
+    context 'when applying a day of month' do
+      let(:expected_time) { Time.zone.parse('2018-08-12 23:10:15') }
+
+      before { with_timestamp.on_day(12) }
+
+      around do |example|
+        klass.timezone = 'Europe/Paris'
+        Time.zone = 'Europe/Paris'
+        Timecop.freeze(Time.zone.parse('2018-07-16 23:10:15'))
+        expected_time
+        example.run
+        Timecop.return
+        Time.zone = nil
+        klass.timezone = nil
+      end
+
+      it { is_expected.to eq([expected_time.to_i, nil]) }
     end
   end
 end

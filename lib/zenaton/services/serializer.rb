bd626e8600d4b8b005e5b75f6e427060a6af9aeb
Igor de Alcantara Barroso <idabmat@gmail.com>
2018-07-27 17:09:42 +0200

    Decode

--- lib/zenaton/services/serializer.rb
+++ lib/zenaton/services/serializer.rb
@@ -37,6 +37,21 @@ module Zenaton
         value.to_json
       end
 
+      def decode(json)
+        parsed_json = JSON.parse(json)
+        @decoded = []
+        @encoded = parsed_json.delete(KEY_STORE)
+        case parsed_json.keys.first
+        when KEY_DATA
+          return parsed_json[KEY_DATA]
+        when KEY_ARRAY
+          return decode_enumerable(parsed_json[KEY_ARRAY])
+        when KEY_OBJECT
+          id = parsed_json[KEY_OBJECT][ID_PREFIX.length..-1].to_i
+          return decode_object(id, @encoded[id])
+        end
+      end
+
       private
 
       def array_type?(data)
@@ -83,6 +98,49 @@ module Zenaton
           }
         end
         "#{ID_PREFIX}#{id}"
+      end
+
+      def object_id?(string)
+        string.is_a?(String) \
+          && string.start_with?(ID_PREFIX) \
+          && string[ID_PREFIX.length..-1].to_i <= @encoded_length
+      end
+
+      def decode_enumerable(enumerable)
+        return decode_array(enumerable) if enumerable.is_a?(Array)
+        return decode_hash(enumerable) if enumerable.is_a?(Hash)
+        raise ArgumentError, 'Unknown type'
+      end
+
+      def decode_array(array)
+        array.map { |elem| decode_element(elem) }
+      end
+
+      def decode_hash(hash)
+        hash.transform_values { |value| decode_element(value) }
+      end
+
+      def decode_element(value)
+        if object_id?(value)
+          id = value[ID_PREFIX.length..-1].to_i
+          encoded = @encoded[id]
+          decode_object(id, encoded) if encoded.is_a?(Hash)
+        elsif value.is_a?(Array)
+          decode_array(value)
+        elsif value.is_a?(Hash)
+          decode_hash(value)
+        else
+          value
+        end
+      end
+
+      def decode_object(id, encoded_object)
+        decoded = @decoded[id]
+        return decoded if decoded
+        object = @properties.blank_instance(encoded_object[KEY_OBJECT_NAME])
+        @decoded[id] = object
+        properties = decode_hash(encoded_object[KEY_OBJECT_PROPERTIES])
+        @properties.set(object, properties)
       end
     end
   end
--- spec/zenaton/services/serializer_spec.rb
+++ spec/zenaton/services/serializer_spec.rb
@@ -130,4 +130,126 @@ RSpec.describe Zenaton::Services::Serializer do
       end
     end
   end
+
+  describe '#decode' do
+    let(:decoded) { serializer.decode(json) }
+
+    context 'with a string do' do
+      let(:json) { { 'd' => 'e', 's' => [] }.to_json }
+
+      it 'returns the string' do
+        expect(decoded).to eq('e')
+      end
+    end
+
+    context 'with an integer do' do
+      let(:json) { { 'd' => 1, 's' => [] }.to_json }
+
+      it 'returns the integer' do
+        expect(decoded).to eq(1)
+      end
+    end
+
+    context 'with a float' do
+      let(:json) { { 'd' => 1.8, 's' => [] }.to_json }
+
+      it 'returns the float' do
+        expect(decoded).to eq(1.8)
+      end
+    end
+
+    context 'with true' do
+      let(:json) { { 'd' => true, 's' => [] }.to_json }
+
+      it 'returns the boolean' do
+        expect(decoded).to eq(true)
+      end
+    end
+
+    context 'with false' do
+      let(:json) { { 'd' => false, 's' => [] }.to_json }
+
+      it 'returns the boolean' do
+        expect(decoded).to eq(false)
+      end
+    end
+
+    context 'with nil' do
+      let(:json) { { 'd' => nil, 's' => [] }.to_json }
+
+      it 'returns the nil object' do
+        expect(decoded).to be_nil
+      end
+    end
+
+    context 'with an array' do
+      let(:json) { { 'a' => [1, 'e'], 's' => [] }.to_json }
+
+      it 'returns the array' do
+        expect(decoded).to eq([1, 'e'])
+      end
+    end
+
+    context 'with a hash' do
+      let(:json) { { 'a' => { 'key' => 'value' }, 's' => [] }.to_json }
+
+      it 'returns the hash' do
+        expect(decoded).to eq('key' => 'value')
+      end
+    end
+
+    context 'with a simple object' do
+      let(:json) do
+        {
+          'o' => '@zenaton#0',
+          's' => [
+            {
+              'n' => 'SerializeMe',
+              'p' => {
+                '@initialized' => true
+              }
+            }
+          ]
+        }.to_json
+      end
+
+      it 'returns a new instance of the object' do
+        expect(decoded).to be_a(SerializeMe)
+      end
+
+      it 'returns an instance with the correct instance variables' do
+        expect(decoded.instance_variable_get(:@initialized)).to \
+          eq(true)
+      end
+    end
+
+    context 'with an object with circular dependencies' do
+      let(:json) do
+        {
+          'o' => '@zenaton#0',
+          's' => [
+            {
+              'n' => 'SerializeCircular::Parent',
+              'p' => {
+                '@child' => '@zenaton#1'
+              }
+            }, {
+              'n' => 'SerializeCircular::Child',
+              'p' => {
+                '@parent' => '@zenaton#0'
+              }
+            }
+          ]
+        }.to_json
+
+        it 'returns the correct object type' do
+          expect(decoded).to be_a(SerializeCircular::Parent)
+        end
+
+        it 'correctly instantes child objects' do
+          expected(decoded.child).to be_a(SerializeCircular::Child)
+        end
+      end
+    end
+  end
 end
